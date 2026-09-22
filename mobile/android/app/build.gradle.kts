bc3fb66f81bee48cc48fef0a0281edb21775ab46
Nicola F. Salvaturi <muhammad.salvaturi@bankmas.co.id>
2026-09-22 11:48:55 +0700

    chore: change package id

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mobile"
+    namespace = "id.nicolafsalv.liveness"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobile"
+        applicationId = "id.nicolafsalv.liveness"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
